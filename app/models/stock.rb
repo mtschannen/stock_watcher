90521108c6772922d96e6fa5d39fc7ff934ba8c1
mtschannen <tschannenmatt@gmail.com>
2017-06-13 16:44:58 -0500

    Removed raw data on show

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,4 @@
 class Stock < ApplicationRecord
-  has_attached_file :stock_logo, styles: { medium: "300x300>", thumb: "300x300>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :stock_logo, styles: { medium: "300x300>", thumb: "500x500>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :stock_logo, content_type: /\Aimage\/.*\z/
 end
