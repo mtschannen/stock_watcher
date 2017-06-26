23b6dc33ffae0b4461f235d7096d281af268db20
Logan Harrison <loganharrison1224@yahoo.com>
2017-06-26 11:20:46 -0500

    commenting

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,6 +2,7 @@ class ResourcesController < ApplicationController
   def index
     @stocks = Stock.where(user_id: session[:current_user_id]).order("ticker_symbol ASC")
 
+    #used to the quick links menu bar will still operate on this page
     @i = 0
     yahoo_client = YahooFinance::Client.new
     @tickers = Array.new
