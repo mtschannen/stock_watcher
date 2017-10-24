ed2812cba15624bf2239d6c52c8f62f07c23f0b4
Unknown <tschannenmatt@gmail.com>
2017-10-24 18:45:04 -0500

    working copy 1

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -101,57 +101,57 @@ class StocksController < ApplicationController
     resp = HTTP.get(url)
     @history = JSON.parse(resp, symbolize_keys: true)
 
-    if !(@history["dataset"].eql? "N/A")
-      # book_values = @history["dataset"]["data"]
-
-      # # derivative FYPM values
-      # v1 = book_values[3][1].to_f - book_values[4][1].to_f
-      # v2 = book_values[2][1].to_f - book_values[3][1].to_f
-      # v3 = book_values[1][1].to_f - book_values[2][1].to_f
-      # v4 = book_values[0][1].to_f - book_values[1][1].to_f
-      # @div = @data[0].dividend_yield.to_f
-      # price = @data[0].last_trade_price.to_f
-      # @five_year_div_yield = ((((@div * 0.01) + 1.0) ** 5.0) - 1.0) * 100.0
-      # # variables for derivative book value linear fit
-      # sigma_x = 6.0
-      # sigma_x_squared = 14.0
-      # sigma_y = v1 + v2 + v3 + v4
-      # sigma_xy = v2 + v3*2.0 + v4*3.0
-      # n = 4.0
-      # # derivative book value linear fit
-      # a = ((sigma_y*sigma_x_squared) - (sigma_x*sigma_xy))/((n*sigma_x_squared)-(sigma_x**2))
-      # b = ((n*sigma_xy) - (sigma_x*sigma_y))/((n*sigma_x_squared)-(sigma_x**2))
-      # five_year_book_value_added = (6.5*b + a)*5.0
-      # @five_year_book_value_yield = (five_year_book_value_added/price)*100
-      # # derivative FYPM final calc
-      # @derivative_fypm.round(2).to_s = (@five_year_book_value_yield + @five_year_div_yield)/10
-
-      # # non- derivative FYPM values
-      # v1 = book_values[4][1].to_f
-      # v2 = book_values[3][1].to_f
-      # v3 = book_values[2][1].to_f
-      # v4 = book_values[1][1].to_f
-      # v5 = book_values[0][1].to_f
-      # # variables for non-derivative book value linear fit
-      # sigma_x = 10.0
-      # sigma_x_squared = 30.0
-      # sigma_y = v1 + v2 + v3 + v4 + v5
-      # sigma_xy = v2 + v3*2.0 + v4*3.0 + v5*4.0
-      # n = 5.0
-      # # non-derivative book value linear fit
-      # a = ((sigma_y*sigma_x_squared) - (sigma_x*sigma_xy))/((n*sigma_x_squared)-(sigma_x**2))
-      # b = ((n*sigma_xy) - (sigma_x*sigma_y))/((n*sigma_x_squared)-(sigma_x**2))
-      # five_year_book_value_added_linear = (10.0*b + a) - v5
-      # five_year_book_value_added_rate = 5.0*b
-      # @five_year_book_value_yield_linear = (five_year_book_value_added_linear/price)*100
-      # @five_year_book_value_yield_rate = (five_year_book_value_added_rate/price)*100
-      # # non-derivative FYPM final calc
-      # @linear_fypm.round(2).to_s = (@five_year_book_value_yield_linear + @five_year_div_yield)/10
-      # @rate_fypm.round(2).to_s = (@five_year_book_value_yield_rate + @five_year_div_yield)/10
-    else
+    if @history["dataset"].nil?
       @derivative_fypm = "N/A"
       @linear_fypm = "N/A"
       @rate_fypm = "N/A"
+    else
+      book_values = @history["dataset"]["data"]
+
+      # derivative FYPM values
+      v1 = book_values[3][1].to_f - book_values[4][1].to_f
+      v2 = book_values[2][1].to_f - book_values[3][1].to_f
+      v3 = book_values[1][1].to_f - book_values[2][1].to_f
+      v4 = book_values[0][1].to_f - book_values[1][1].to_f
+      @div = @data[0].dividend_yield.to_f
+      price = @data[0].last_trade_price.to_f
+      @five_year_div_yield = ((((@div * 0.01) + 1.0) ** 5.0) - 1.0) * 100.0
+      # variables for derivative book value linear fit
+      sigma_x = 6.0
+      sigma_x_squared = 14.0
+      sigma_y = v1 + v2 + v3 + v4
+      sigma_xy = v2 + v3*2.0 + v4*3.0
+      n = 4.0
+      # derivative book value linear fit
+      a = ((sigma_y*sigma_x_squared) - (sigma_x*sigma_xy))/((n*sigma_x_squared)-(sigma_x**2))
+      b = ((n*sigma_xy) - (sigma_x*sigma_y))/((n*sigma_x_squared)-(sigma_x**2))
+      five_year_book_value_added = (6.5*b + a)*5.0
+      @five_year_book_value_yield = (five_year_book_value_added/price)*100
+      # derivative FYPM final calc
+      @derivative_fypm.round(2).to_s = (@five_year_book_value_yield + @five_year_div_yield)/10
+
+      # non- derivative FYPM values
+      v1 = book_values[4][1].to_f
+      v2 = book_values[3][1].to_f
+      v3 = book_values[2][1].to_f
+      v4 = book_values[1][1].to_f
+      v5 = book_values[0][1].to_f
+      # variables for non-derivative book value linear fit
+      sigma_x = 10.0
+      sigma_x_squared = 30.0
+      sigma_y = v1 + v2 + v3 + v4 + v5
+      sigma_xy = v2 + v3*2.0 + v4*3.0 + v5*4.0
+      n = 5.0
+      # non-derivative book value linear fit
+      a = ((sigma_y*sigma_x_squared) - (sigma_x*sigma_xy))/((n*sigma_x_squared)-(sigma_x**2))
+      b = ((n*sigma_xy) - (sigma_x*sigma_y))/((n*sigma_x_squared)-(sigma_x**2))
+      five_year_book_value_added_linear = (10.0*b + a) - v5
+      five_year_book_value_added_rate = 5.0*b
+      @five_year_book_value_yield_linear = (five_year_book_value_added_linear/price)*100
+      @five_year_book_value_yield_rate = (five_year_book_value_added_rate/price)*100
+      # non-derivative FYPM final calc
+      @linear_fypm.round(2).to_s = (@five_year_book_value_yield_linear + @five_year_div_yield)/10
+      @rate_fypm.round(2).to_s = (@five_year_book_value_yield_rate + @five_year_div_yield)/10
     end
   end
   helper_method :get_basic_info
