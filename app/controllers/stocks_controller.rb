9b1ff190fdfec2e43356cc8067e06815caede50b
Unknown <tschannenmatt@gmail.com>
2017-10-23 21:33:35 -0500

    wrk plz

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -98,7 +98,7 @@ class StocksController < ApplicationController
       format.html { @data }
     end
     yahoo_client_two = YahooFinance::Client.new
-    @book_values = yahoo_client_two.historical_quotes([@stock.ticker_symbol], { period: :monthly })
+    @book_values = yahoo_client_two.historical_quotes("AAPL", { period: :monthly })
   end
   helper_method :get_basic_info
 
