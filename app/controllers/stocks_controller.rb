edc2c54e445c1452228d282316ef218bad771755
Unknown <tschannenmatt@gmail.com>
2017-10-27 21:41:14 -0500

    Attempt to fix EOM error

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -29,13 +29,13 @@ class StocksController < ApplicationController
     opts[:subject]     ||= "FYPM alert for " + stock_ticker + "!"
     opts[:body]        ||= stock_ticker + "\'s FYPM is up " + fypm_change + " today.\n\n-Obduros"
 
-    msg = <<END_OF_MESSAGE
-    From: #{opts[:from_alias]} <#{opts[:from]}>
-    To: <#{to}>
-    Subject: #{opts[:subject]}
+    msg = "<<END_OF_MESSAGE
+  From: #{opts[:from_alias]} <#{opts[:from]}>
+  To: <#{to}>
+  Subject: #{opts[:subject]}
 
-    #{opts[:body]}
-    END_OF_MESSAGE
+  #{opts[:body]}
+  END_OF_MESSAGE"
 
     Net::SMTP.start(opts[:server]) do |smtp|
       smtp.send_message msg, opts[:from], to
